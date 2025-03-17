9d8e6cf85fc42f8630621fef7f6eec6aa6c079c7
jojocoder28 <dasjojo7@gmail.com>
2025-03-17 22:25:36 +0530

    headershown false

--- VirtualClassroom/app/(redux)/AppWrapper.js
+++ VirtualClassroom/app/(redux)/AppWrapper.js
@@ -17,8 +17,11 @@ function AppWrapper() {
         name="index"
         options={{ title: "Home", headerShown: false }}
       />
-      <Stack.Screen name="profile" options={{ title: "Profile" }} />
-      <Stack.Screen name="auth/login" options={{ title: "Login" }} />
+      
+      <Stack.Screen name="profile" options={{ title: "Profile", headerShown: false }} />
+      <Stack.Screen name="auth/login" options={{ title: "Login", headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ title: "Virtual Classroom", headerShown: false }} />
+      <Stack.Screen name="auth/register" options={{ title: "register", headerShown: false }} />
     </Stack>
   );
 }
