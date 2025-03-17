d4879835cda128876a1fb97afa918fab885f2276
jojocoder28 <dasjojo7@gmail.com>
2025-03-17 21:28:45 +0530

    authentication added

---
+++ VirtualClassroom/app/(tabs)/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Fontisto from "@expo/vector-icons/Fontisto";
+
+const TabHome = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Chop The Data</Text>
+    </View>
+  );
+};
+
+export default TabHome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#666",
+  },
+  techList: {
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+  techItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 15,
+    elevation: 5,
+  },
+  techText: {
+    fontSize: 18,
+    color: "#fff",
+    marginLeft: 10,
+    fontWeight: "bold",
+  },
+});
