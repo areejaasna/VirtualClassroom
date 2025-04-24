4af41e1de81e8a671bf0ca3bcff9fdd91981cb17
oindrilapathak2020 <oindrilapathak2020@gmail.com>
2025-04-24 11:35:19 +0000

    Modified user.js

--- Backend/model/User.js
+++ Backend/model/User.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    firstName: { type: String, required: true }, // Added firstName
+    lastName: { type: String, required: true }, // Added lastName
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true }, // Added unique constraint for email
     password: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ['Student', 'Teacher', 'Admin'], // Added role with enum validation
+      default: 'Student' // Optional: set a default role
+    },
+    college: { type: String, required: true }, // Added college
   },
   {
     timestamps: true,
